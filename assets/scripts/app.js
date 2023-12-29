3d15c4aef09be12c48bac7c1f8e2494e3a536ce3
AShvab <shvabovska@gmail.com>
2023-12-29 23:46:28 +0200

    add some functions

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -4,8 +4,48 @@ const backdrop = document.getElementById("backdrop");
 const cancelAddMovieButton = addMovieModal.querySelector(".btn--passive");
 const confirmAddMovieButton = addMovieModal.querySelector(".btn--success");
 const userInputs = addMovieModal.querySelectorAll("input");
+const entryTextSection = document.getElementById("entry-text");
 
 const movies = [];
+
+const updateUI = () => {
+  if (movies.length === 0) {
+    entryTextSection.style.display = "block";
+  } else {
+    entryTextSection.style.display = "none";
+  }
+};
+
+const deleteMovieHandler = (movieId) => {
+  let movieIndex = 0;
+  for (const movie of movies) {
+    if (movie.id === movieId) {
+      break;
+    }
+    movieIndex++;
+  }
+  movies.splice(movieIndex, 1);
+  const listRoot = document.getElementById("movie-list");
+  listRoot.children[movieIndex].remove();
+};
+
+const renderNewMovieElement = (id, title, imageUrl, rating) => {
+  const newMovieElement = document.createElement("li");
+  newMovieElement.className = "movie-element";
+  newMovieElement.innerHTML = `
+<div class="movie-element__image">
+<img src="${imageUrl}" alt="${title}" >
+</div>
+
+<div class="movie-element__info">
+<h2>${title}</h2>
+<p>${rating}/5 stars</p>
+</div>
+`;
+  newMovieElement.addEventListener("click", deleteMovieHandler.bind(null, id));
+  const listRoot = document.getElementById("movie-list");
+  listRoot.appendChild(newMovieElement);
+};
 
 const toggleBackdrop = () => {
   backdrop.classList.toggle("visible");
@@ -50,6 +90,7 @@ const addMovieHandler = () => {
     return;
   }
   const newMovie = {
+    id: Math.random().toString(),
     title: titleValue,
     image: imageUrlValue,
     rating: ratingValue,
@@ -58,6 +99,13 @@ const addMovieHandler = () => {
   console.log(movies);
   toggleMovieModal();
   clearMovieInput();
+  renderNewMovieElement(
+    newMovie.id,
+    newMovie.title,
+    newMovie.image,
+    newMovie.rating
+  );
+  updateUI();
 };
 
 startAddMovieButton.addEventListener("click", toggleMovieModal);
